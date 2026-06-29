7f1b34501f1d877791a08045926a257495e446a3
Samuel Cristhian <samuel.cristhian@aluno.uece.br>
2026-06-29 12:15:40 -0300

    Realizando os commands de login, locators e o arquivo de teste baseado nos casos de teste

---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,38 @@
+import { loginLocators } from '../support/locators/login';
+
+describe('Login', () => {
+  beforeEach(() => {
+    cy.visit('https://www.saucedemo.com/');
+  });
+
+  it('LG-01 - Login com usuário válido', () => {
+    cy.fixture('login').then((user) => {
+      cy.login(user.usernameValid, user.passwordValid);
+
+      cy.get(loginLocators.productsTitle).should('contain.text', 'Products');
+      cy.url().should('include', '/inventory.html');
+    });
+  });
+
+  it('LG-02 - Login com usuário inválido', () => {
+    cy.fixture('login').then((user) => {
+      cy.login(user.usernameInvalid, user.passwordValid);
+
+      cy.get(loginLocators.errorMessage).should('contain.text', 'Username and password do not match any user in this service');
+    });
+  });
+
+  it('LG-03 - Login com senha incorreta', () => {
+    cy.fixture('login').then((user) => {
+      cy.login(user.usernameValid, user.passwordInvalid);
+
+      cy.get(loginLocators.errorMessage).should('contain.text', 'Username and password do not match any user in this service');
+    });
+  });
+
+  it('LG-04 - Login com campos vazios', () => {
+    cy.login('','');
+
+    cy.get(loginLocators.errorMessage).should('contain.text', 'Username is required');
+  });
+});
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,25 +1,15 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+import { loginLocators } from '../support/locators/login';
+
+Cypress.Commands.add('login', (username, password) => {
+  
+  //verificar se username e password são fornecidos antes de tentar digitar
+    if (username) {
+    cy.get(loginLocators.usernameInput).type(username);
+  }
+
+  if (password) {
+    cy.get(loginLocators.passwordInput).type(password);
+  }
+  
+  cy.get(loginLocators.loginButton).click();
+});
